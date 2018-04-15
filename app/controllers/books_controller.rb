8f0e425249e03b2c417f2f0f7e448fd913ec2b42
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-15 13:05:20 -0400

    Implement book inventory listing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,8 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
-  # GET /books.json
   def index
-    @books = Book.all
+    @books = current_user.books
   end
 
   # GET /books/1
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # associations
+  it { is_expected.to have_many(:books).dependent(:delete_all) }
+end
